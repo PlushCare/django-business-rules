6c64fbf5e4c32d61d941f649f830237da1b18ea8
Candice Lin <clin@plushcare.com>
2021-05-27 14:18:30 -0700

    remove some stuff

--- dbr/django_business_rules/models.py
+++ dbr/django_business_rules/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-try:
-    # django < 1.10
-    from django.core.urlresolvers import reverse
-except ImportError:
-    # django >= 1.10
-    from django.urls import reverse
-from django.utils.encoding import python_2_unicode_compatible
+
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as __
 
 
-@python_2_unicode_compatible
 class BusinessRuleModel(models.Model):
     name = models.TextField(unique=True, verbose_name=__('name'))
     description = models.TextField(blank=True, verbose_name=__('description'))
