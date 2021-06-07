9f0824815fdec920542c7006783dba736205c153
Candice Lin <clin@plushcare.com>
2021-06-07 10:56:55 -0700

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-business-rules',
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
     license='BSD License',
