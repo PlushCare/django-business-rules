ae739a9dc6d7bc9b272d9db67849603ea429362a
Candice Lin <clin@plushcare.com>
2021-06-09 10:29:14 -0700

    make edit admin only

--- django_business_rules/urls.py
+++ django_business_rules/urls.py
@@ -3,10 +3,13 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 
 from django_business_rules.views import BusinessRuleFormView, BusinessRuleListView
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 app_name = 'django_business_rules'
 urlpatterns = [
-    url(r'^business-rule/$', BusinessRuleListView.as_view(), name='business-rule-list'),
-    url(r'^business-rule/(?P<pk>[0-9]+)/$', BusinessRuleFormView.as_view(), name='business-rule-form'),
+    url(r'^business-rule/$', staff_member_required(BusinessRuleListView.as_view()),
+        name='business-rule-list'),
+    url(r'^business-rule/(?P<pk>[0-9]+)/$',
+        staff_member_required(BusinessRuleFormView.as_view()), name='business-rule-form'),
 ]
